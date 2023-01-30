8ee6968be13085f425ffae4d70c0a20c60e507b4
SHAH-1846 <118044116+SHAH-1846@users.noreply.github.com>
2023-01-30 22:39:52 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    mongoURL:'mongodb+srv://nasa-api:h26kQDJRNRYMMpuM@nasa-cluster.sgxfc3u.mongodb.net/nasa?retryWrites=true&w=majority',
-    secretorKey: 'SecretKey'
+    mongoURL:'mongo_url',
+    secretorKey: 'secret_key'
 };
